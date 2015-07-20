03c6c228ec0be4b4a1a27883f4a9b4af43bc7a79
kv <krisviceral@gmail.com>
2015-07-19 22:52:12 -0400

    Initial commit

---
+++ src/test/java/kata/KataApplicationTests.java
@@ -0,0 +1,16 @@
+package kata;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = KataApplication.class)
+public class KataApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
